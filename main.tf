b28820146cef3067cc8c0f110fd99be251630abd
Enrique Zetina <enrique.zetina@grainger.com>
2025-04-22 10:38:56 -0500

    Add security groups

--- main.tf
+++ main.tf
@@ -64,3 +64,42 @@ resource "aws_route_table_association" "private_db_c" {
   subnet_id      = aws_subnet.private_db_c.id
   route_table_id = aws_route_table.private_rt.id
 }
+
+# EC2 Pool Security Group (referenced by MySQL security group)
+resource "aws_security_group" "ec2_pool" {
+  name        = "ec2_pool"
+  description = "Security group for EC2 instances"
+  vpc_id      = aws_vpc.main.id
+
+  tags = {
+    Name = "${var.environment}-ec2-pool"
+  }
+}
+
+# MySQL Security Group
+resource "aws_security_group" "mysql" {
+  name        = "mysql"
+  description = "Defines access to ghost db"
+  vpc_id      = aws_vpc.main.id
+
+  # Ingress rule for MySQL access from EC2 pool
+  ingress {
+    description     = "MySQL access from EC2 instances"
+    from_port       = 3306
+    to_port         = 3306
+    protocol        = "tcp"
+    security_groups = [aws_security_group.ec2_pool.id]
+  }
+
+  # Egress rule - allow all outbound traffic
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.environment}-mysql"
+  }
+}
--- outputs.tf
+++ outputs.tf
@@ -13,3 +13,11 @@ output "private_db_subnet_ids" {
 output "private_route_table_id" {
   value = aws_route_table.private_rt.id
 }
+
+output "mysql_security_group_id" {
+  value = aws_security_group.mysql.id
+}
+
+output "ec2_pool_security_group_id" {
+  value = aws_security_group.ec2_pool.id
+}
